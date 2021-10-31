4858e7a62e94784aca4a8b798bed500d02a75f73
A7abhilash <a7abhilash@gmail.com>
2021-10-31 15:57:47 +0530

    bc, admin: create-candidate

--- src/contracts/VotingApp.sol
+++ src/contracts/VotingApp.sol
@@ -2,19 +2,72 @@ pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract VotingApp{
+	// Admin
 	string public name;
 	string public adminGoogleId;
 	address public admin;
+
+	// Store phase
+	uint public phase;
+	/*
+		0: Election is announced to be held and candidates can be created by admin
+		1: Registration - Voters can register
+		2: Voting - Voters are eligigle to vote. No new candidates!
+		3: Results - No more voting!
+	*/
+
+	// Store voters
+	mapping(address => bool) public voters;
+
+	// Store candidates
+	uint public candidatesCount;
+	mapping(uint => Candidate) public candidates;
+	struct Candidate{
+		uint candidateId;
+		string name;
+		uint votesCount;
+	}
 
 	constructor() public{
 		name = "VotingApp";
 		admin = msg.sender;
 		adminGoogleId = "116975414122189328909";
+
+		phase = 0;
 	}
 
-	/* Modifier: Only Admin */
+	/* Modifiers */
 	modifier onlyAdmin{
 		require(msg.sender == admin, "Only admin is allowed to perform this action");
 		_;
+	}
+
+	/* Events */
+	event PhaseChanged(uint phase);
+	event CandidateCreated(uint indexed candidateId, string name, uint votesCount);
+	event CastedVote(uint indexed candidateId, string hash, uint votesCount);
+
+	/* Functions */
+	function createCandidate(string memory name) public onlyAdmin {
+		// Make sure phase is right
+		require(phase == 0, "Candidate Registration phase is over!");
+
+		// Make sure the inputs exists
+		require(bytes(name).length > 0, "Candidate name is required!");
+
+		// Add candidate
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
+
+		// Trigger event
+		emit CandidateCreated(candidatesCount, name, 0);
+	}
+
+	function changePhase() public onlyAdmin{
+		require(phase <= 3, "Election is over!");
+
+		phase++;
+
+		emit PhaseChanged(phase);
 	}
 }
--- test/test.js
+++ test/test.js
@@ -4,8 +4,7 @@ require("chai").use(require("chai-as-promised")).should();
 
 contract("VotingApp", (accounts) => {
   let votingApp;
-  const [deployer, voter1, voter2, candidate1, candidate2, candidate3] =
-    accounts;
+  const [deployer, voter1, voter2, voter3, voter4, voter5] = accounts;
 
   before(async () => {
     votingApp = await VotingApp.deployed();
@@ -31,5 +30,53 @@ contract("VotingApp", (accounts) => {
       const adminGoogleId = await votingApp.adminGoogleId();
       assert.equal(adminGoogleId, "116975414122189328909");
     });
+
+    it("initially, phase is 0", async () => {
+      const phase = await votingApp.phase();
+      assert.equal(phase.toNumber(), 0);
+    });
   });
+
+  describe("admin functions", async () => {
+    let result, candidatesCount, phase;
+
+    before(async () => {
+      phase = await votingApp.phase();
+
+      await votingApp.createCandidate("Candidate 1", { from: deployer });
+      await votingApp.createCandidate("Candidate 2", { from: deployer });
+      result = await votingApp.createCandidate("Candidate 3", {
+        from: deployer,
+      });
+      candidatesCount = await votingApp.candidatesCount();
+    });
+
+    it("creates candidate", async () => {
+      /*  SUCCESS */
+      assert.equal(phase.toNumber(), 0, "it is registration phase");
+      assert.equal(
+        candidatesCount.toNumber(),
+        3,
+        "no. of candidates is correct"
+      );
+
+      const event = result.logs[0].args;
+      assert.equal(event.candidateId, 3, "candidate id is correct");
+      assert.equal(event.name, "Candidate 3", "candidate name is correct");
+      assert.equal(event.votesCount, 0, "candidate votes count is correct");
+
+      /*  FAILURE */
+      // Only admin can create new candidate
+      await votingApp.createCandidate("Candidate 8", { from: voter2 }).should.be
+        .rejected;
+
+      // Candidate name is required
+      await votingApp.createCandidate("", { from: deployer }).should.be
+        .rejected;
+    });
+  });
+
+  // // Registration phase is not started
+  // await votingApp.createCandidate("Candidate 8", { from: deployer }).should
+  //   .be.rejected;
 });
